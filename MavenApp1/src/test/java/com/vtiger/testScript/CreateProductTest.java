dde88b33921cfd7d8b9692ccccec9ee8406acac6
Muzna123 <muznaafreen@gmail.com>
2020-03-18 00:17:17 +0530

    empty product page created

---
+++ MavenApp1/src/test/java/com/vtiger/testScript/CreateProductTest.java
@@ -0,0 +1,11 @@
+package com.vtiger.testScript;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+	@Test
+	public void createProductTest() {
+		System.out.println("Will create later");
+	}
+
+}
