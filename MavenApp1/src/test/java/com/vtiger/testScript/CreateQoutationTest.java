70c5f0c4e756d7fccabbbd627610c1af28294f37
Muzna123 <muznaafreen@gmail.com>
2020-03-18 00:10:27 +0530

    Have created empty CreateQuotationtest

---
+++ MavenApp1/src/test/java/com/vtiger/testScript/CreateQoutationTest.java
@@ -0,0 +1,11 @@
+package com.vtiger.testScript;
+
+import org.testng.annotations.Test;
+
+public class CreateQoutationTest {
+	@Test
+	public void createQoutationTest() {
+		System.out.println("I will create Quotation  later");
+	}
+
+}
